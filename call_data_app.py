1106e94202fa0820ce4729f4882784e3aef03214
nitishgupta4291 <nitishgupta4291@gmail.com>
2023-09-25 20:25:11 +0530

    Update call_data_app.py

--- call_data_app.py
+++ call_data_app.py
@@ -8,7 +8,7 @@ This is a temporary script file.
 import streamlit as st
 import pandas as pd
 from io import StringIO
-st.header('CALL CENTER ANALYTICS')
+st.header('Best Time To Call Prediction APP')
 
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
